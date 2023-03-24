f349b18b2cbd9eadef7d3769d90ba6cc341e3f48
Aramik <aramikm@gmail.com>
2023-03-24 14:16:09 -0700

    some improvements

--- core/src/dsnp/pseudo_relationship_identifier.rs
+++ core/src/dsnp/pseudo_relationship_identifier.rs
@@ -1,8 +1,11 @@
 use crate::dsnp::dsnp_types::DsnpPrid;
-use crypto_box::{aead::AeadInPlace, Nonce, PublicKey, SalsaBox, SecretKey};
+use crypto_box::{aead::AeadInPlace, Nonce, PublicKey, SecretKey};
 use hkdf::Hkdf;
 use sha2::Sha256;
+use std::ops::Deref;
 use x25519_dalek::x25519;
+use xsalsa20poly1305::{KeyInit, XSalsa20Poly1305};
+use zeroize::{Zeroize, Zeroizing};
 
 const PRI_CONTEXT: &[u8] = b"PRIdCtx0";
 struct PseudonymousRelationshipIdentifier;
@@ -11,22 +14,22 @@ impl PseudonymousRelationshipIdentifier {
 	fn create(a: u64, b: u64, a_secret_key: &SecretKey, b_public_key: &PublicKey) -> DsnpPrid {
 		let id_b = b.to_le_bytes();
 
-		let root_shared = x25519(*a_secret_key.as_bytes(), *b_public_key.as_bytes());
-		let hk = Hkdf::<Sha256>::new(Some(&id_b[..]), &root_shared);
-		let mut derived_key = [0u8; 64];
-		hk.expand(PRI_CONTEXT, &mut derived_key).unwrap();
+		let mut root_shared =
+			Zeroizing::new(x25519(*a_secret_key.as_bytes(), *b_public_key.as_bytes()));
+		let hk = Hkdf::<Sha256>::new(Some(&id_b[..]), root_shared.as_slice());
+		let mut derived_key = Zeroizing::new([0u8; 32]);
+		hk.expand(PRI_CONTEXT, derived_key.as_mut_slice()).unwrap();
+		root_shared.zeroize();
 
-		let mut public_key_arr = [0u8; 32];
-		let mut secret_key_arr = [0u8; 32];
-		public_key_arr.copy_from_slice(&derived_key[..32]);
-		secret_key_arr.copy_from_slice(&derived_key[32..]);
-		let salsa = SalsaBox::new(&public_key_arr.into(), &secret_key_arr.into());
+		let salsa = XSalsa20Poly1305::new(derived_key.deref().into());
 
 		let mut nonce = [0u8; 24];
 		nonce.copy_from_slice(&[&[0u8; 16], &id_b[..]].concat());
 		let nonce = Nonce::from(nonce);
+
 		let mut buffer = a.to_le_bytes();
 		salsa.encrypt_in_place_detached(&nonce, &[], &mut buffer).unwrap();
+		derived_key.zeroize();
 
 		DsnpPrid::new(&buffer)
 	}
