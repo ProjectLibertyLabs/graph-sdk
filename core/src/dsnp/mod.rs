3faa477d7d0c65ccaae6f44af6b7c8309fbaeb51
Joe Caputo <joseph.caputo@unfinished.com>
2023-04-04 15:30:10 -0400

    feat(5): lint

--- core/src/dsnp/mod.rs
+++ core/src/dsnp/mod.rs
@@ -2,8 +2,8 @@ pub mod api_types;
 pub mod compression;
 pub mod dsnp_types;
 pub mod encryption;
-mod pseudo_relationship_identifier;
 pub mod graph_page;
+mod pseudo_relationship_identifier;
 pub mod reader_writer;
 pub mod schema;
 #[cfg(test)]
