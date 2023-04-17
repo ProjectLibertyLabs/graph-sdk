16870ba26c42931edddb0218264a0f177103451a
Joe Caputo <joseph.caputo@unfinished.com>
2023-04-17 16:52:43 -0400

    feat(refactor configs): refactor config into separate crate, and fix infinite loop bug in export (#33)
    Refactor configuration data (chain constants, schema ids & schemas) into
    their own independent crate
    
    # Checklist
    - [x] Tests added

--- core/src/frequency/mod.rs
+++ core/src/frequency/mod.rs
@@ -1,36 +1,23 @@
-pub mod config;
 pub mod reader_writer;
 
-use crate::dsnp::api_types::Config;
+use dsnp_graph_config::{Config, MAINNET_CONFIG, ROCOCO_CONFIG};
 
 #[allow(dead_code)] // todo: remove
 /// A utility to read/write data from and to Frequency chain specific implementation of DSNP
 pub struct Frequency {
-	config: Box<dyn Config>,
+	config: Config,
 }
 
-/// Macro to create a Frequency instance with the appropriate config
-///
-/// Usage:
-/// ```ignore
-/// frequency!(ConfigMain); // instantiate Frequency for Mainnet
-/// frequency!(ConfigRococo); // instantiate Frequency for Rococo
-/// frequency!(ConfigDev); // instantiate Frequency, using environment (see below)
-/// frequency!(ConfigDev { [(ConnectionType::Follow(PrivacyType::Public), /* schema id */), ... ]}); // instantiate Frequency with code-defined schema id values
-/// ```
-#[macro_export]
-macro_rules! frequency {
-	(ConfigMain) => {
-		Frequency { config: Box::new(ConfigMain {}) }
-	};
-	(ConfigRococo) => {
-		Frequency { config: Box::new(ConfigRococo {}) }
-	};
-	(ConfigDev) => {
-		Frequency { config: Box::new(ConfigDev::new(None)) }
-	};
-	(ConfigDev { $map_values:expr }) => {{
-		let map = HashMap::from($map_values);
-		Frequency { config: Box::new(ConfigDev::new(Some(map))) }
-	}};
+impl Frequency {
+	pub fn init(config: Config) -> Self {
+		Frequency { config }
+	}
+
+	pub fn with_mainnet() -> Result<Self, ()> {
+		Ok(Self::init(MAINNET_CONFIG.try_into().map_err(|_| ())?))
+	}
+
+	pub fn with_rococo() -> Result<Self, ()> {
+		Ok(Self::init(ROCOCO_CONFIG.try_into().map_err(|_| ())?))
+	}
 }
