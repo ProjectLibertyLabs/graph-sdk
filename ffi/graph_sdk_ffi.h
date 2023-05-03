cbfd3dd269df7b97400a0e32655eb43d5202c58f
Puneet Saraswat <puneet.saraswat10074@gmail.com>
2023-05-02 22:17:34 -0500

    update header file

--- ffi/graph_sdk_ffi.h
+++ ffi/graph_sdk_ffi.h
@@ -3,7 +3,19 @@
 
 #include <stdint.h>
 
-int32_t graph_sdk_init(const char* config_file_path);
-int32_t graph_sdk_perform_operation(int32_t param);
+typedef struct GraphState GraphState;
+typedef struct UsersData UsersData;
+typedef struct UserIds UserIds;
+typedef struct Environment Environment;
+
+Environment* graph_environment_new(const char* config_file_path);
+void graph_environment_free(Environment* environment);
+
+GraphState* graph_state_new(Environment* environment);
+void graph_state_free(GraphState* graph_state);
+
+int32_t graph_state_contains_user_graph(GraphState* graph_state, uint64_t user_id);
+size_t graph_state_len(GraphState* graph_state);
+void graph_state_remove_user_graph(GraphState* graph_state, uint64_t user_id);
 
 #endif // GRAPH_SDK_FFI_H
