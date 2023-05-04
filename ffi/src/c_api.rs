3329abd366582f93d9bdc5a3c36a2e4b25a0fff1
Puneet Saraswat <puneet.saraswat10074@gmail.com>
2023-05-04 10:30:11 -0500

    add needed functions to c interface

--- ffi/src/c_api.rs
+++ ffi/src/c_api.rs
@@ -1,9 +1,13 @@
 use crate::bindings::*;
-use libc::{c_int, size_t};
+use libc::{c_int, c_void, size_t};
 
 // Define a C-compatible representation of GraphAPI
 #[repr(C)]
 pub struct GraphAPI {
+	graph_state_new: extern "C" fn(environment: *const Environment) -> *mut c_void,
+	graph_state_free: extern "C" fn(*mut c_void),
+	graph_state_with_capacity: extern "C" fn(*const Environment, size_t) -> *mut c_void,
+	graph_state_get_capacity: extern "C" fn(*const c_void) -> size_t,
 	// Use raw pointers instead of references, since references are not C-compatible
 	contains_user_graph: extern "C" fn(*const u64) -> bool,
 	len: extern "C" fn() -> usize,
