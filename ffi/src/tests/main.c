ab9bb7227ebea4cd81398d536c1e2fbb9ce305b5
Puneet Saraswat <puneet.saraswat10074@gmail.com>
2023-05-05 10:16:22 -0500

    start simple

--- ffi/src/tests/main.c
+++ ffi/src/tests/main.c
@@ -1,30 +1,7 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <dlfcn.h>
 #include "graph_sdk_ffi.h"
-typedef void (*print_hello_graph_t)(void);
 
 int main() {
-    // Load the Rust library
-    void* handle = dlopen("libgraph_sdk_ffi.so", RTLD_LAZY);
-    if (!handle) {
-        printf("Error loading library: %s\n", dlerror());
-        exit(1);
-    }
-
-    // Get a pointer to the print_hello_graph function
-    print_hello_graph_t print_hello_graph_func = (print_hello_graph_t) dlsym(handle, "print_hello_graph");
-    const char* dlsym_error = dlerror();
-    if (dlsym_error) {
-        printf("Error getting symbol: %s\n", dlsym_error);
-        dlclose(handle);
-        exit(1);
-    }
-
-    // Call the print_hello_graph function
-    print_hello_graph_func();
-
-    // Unload the Rust library
-    dlclose(handle);
+    print_hello_graph();
     return 0;
 }
