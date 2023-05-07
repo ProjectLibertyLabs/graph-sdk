19691e80534c549218fb1001a46e3e58d8da0945
Joe Caputo <joseph.caputo@unfinished.com>
2023-05-07 17:06:39 -0400

    feat: generate page capacities with 1000x iterations

--- core/src/graph/page_capacities.rs
+++ core/src/graph/page_capacities.rs
@@ -1,15 +1,10 @@
-use dsnp_graph_config::{ConnectionType, PrivacyType};
+use dsnp_graph_config::{ConnectionType, ConnectionType::Follow, ConnectionType::Friendship, PrivacyType::Public, PrivacyType::Private};
 use lazy_static::lazy_static;
 use std::collections::hash_map::*;
 
 lazy_static! {
 	pub static ref PAGE_CAPACITIY_MAP: HashMap<ConnectionType, usize> = {
-		let m = HashMap::from([
-			(ConnectionType::Friendship(PrivacyType::Public), 94),
-			(ConnectionType::Follow(PrivacyType::Public), 93),
-			(ConnectionType::Follow(PrivacyType::Private), 88),
-			(ConnectionType::Friendship(PrivacyType::Private), 50),
-		]);
+		let m = HashMap::from([(Friendship(Public), 92), (Follow(Private), 88), (Friendship(Private), 49), (Follow(Public), 93)]);
 		m
 	};
 }
