f260240c8dcebb0c86c0fdfa00c31e83e5f24b6f
Puneet Saraswat <puneet.saraswat10074@gmail.com>
2023-05-18 14:10:18 -0500

    base work for graph ffi result

--- bridge/ffi/src/lib.rs
+++ bridge/ffi/src/lib.rs
@@ -9,6 +9,7 @@
 //! To use the FFI layer, you need to build the dynamic library (e.g., .so, .dylib, or .dll) by adding
 //! the appropriate configuration to `Cargo.toml`. Then, include the generated dynamic library and
 //! the C header file `dsnp_graph_sdk_ffi.h` in your C, C++, or other FFI-compatible projects.
+
 mod c_api;
 pub use c_api::*;
 mod bindings;
@@ -18,3 +19,53 @@ pub use utils::*;
 
 #[cfg(test)]
 mod tests;
+
+use dsnp_graph_config::errors::{DsnpGraphError, DsnpGraphResult};
+
+#[repr(C)]
+pub struct GraphFFIResult<T> {
+	pub result: *mut T,
+	pub error: *mut DsnpGraphError,
+}
+
+impl<T> GraphFFIResult<T> {
+	pub fn new(result: T) -> Self {
+		Self { result: Box::into_raw(Box::new(result)), error: std::ptr::null_mut() }
+	}
+
+	pub fn new_error(error: DsnpGraphError) -> Self {
+		Self { result: std::ptr::null_mut(), error: Box::into_raw(Box::new(error)) }
+	}
+}
+
+impl<T> Drop for GraphFFIResult<T> {
+	fn drop(&mut self) {
+		if !self.result.is_null() {
+			unsafe { Box::from_raw(self.result) };
+		}
+		if !self.error.is_null() {
+			unsafe { Box::from_raw(self.error) };
+		}
+	}
+}
+
+impl<T> From<DsnpGraphResult<T>> for GraphFFIResult<T> {
+	fn from(result: DsnpGraphResult<T>) -> Self {
+		match result {
+			Ok(result) => Self::new(result),
+			Err(error) => Self::new_error(error),
+		}
+	}
+}
+
+impl<T> Into<DsnpGraphResult<T>> for GraphFFIResult<T> {
+	fn into(self) -> DsnpGraphResult<T> {
+		if !self.result.is_null() {
+			unsafe { Ok(*Box::from_raw(self.result)) }
+		} else if !self.error.is_null() {
+			unsafe { Err(*Box::from_raw(self.error)) }
+		} else {
+			Err(DsnpGraphError::Unknown(anyhow::anyhow!("GraphFFIResult is null")))
+		}
+	}
+}
