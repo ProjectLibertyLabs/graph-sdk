1d2e7c88cefc611b531aa3fef4a4164b9744a2c6
Aramik <aramikm@gmail.com>
2023-05-30 13:09:47 -0700

    implemented jni apis (#89)
    # Goal
    The goal of this PR is implemented rest of JNI apis
    
    Closes #83 
    
    # Discussion
    - implements mapping between Jni, Proto and Rust types
    - Some minor cleanup in Java side for better usability
    - Merging this would feature complete the SDK but there are some
    leftovers like integrating exception handling in JNI and ...
    - Memory allocation and removal seems to be working as expected but will
    need to do more memory specific testing in future to make sure
    # Checklist
    - [x] All apis are tested and works as expected. Only
    `oneSidedFriendship` is not tested since it takes more time to setup the
    test and will be done in future PR (not a blocker to close this)

--- bridge/jni/src/api.rs
+++ bridge/jni/src/api.rs
@@ -1,11 +1,19 @@
-use crate::mappings::map_to_environment;
-use dsnp_graph_core::api::api::GraphState;
+use crate::mappings::{
+	convert_jboolean, map_to_actions, map_to_environment, map_to_imports, serialize_config,
+	serialize_dsnp_users, serialize_graph_edges, serialize_graph_updates, serialize_public_keys,
+};
+use dsnp_graph_config::{DsnpUserId, SchemaId};
+use dsnp_graph_core::api::api::{GraphAPI, GraphState};
 use jni::{
 	objects::{JByteArray, JClass, JObject, JString},
-	sys::{jboolean, jint, jlong},
+	sys::{jboolean, jint, jlong, JNI_ERR, JNI_FALSE},
 	JNIEnv,
 };
-use std::{panic, sync::Mutex};
+use std::{
+	ops::{Deref, DerefMut},
+	panic,
+	sync::Mutex,
+};
 
 // Collection of GraphStates
 static GRAPH_STATES: Mutex<Vec<jlong>> = Mutex::new(Vec::new());
@@ -76,120 +84,295 @@ pub unsafe extern "C" fn Java_io_amplica_graphsdk_Native_freeGraphState<'local>(
 
 #[no_mangle]
 pub unsafe extern "C" fn Java_io_amplica_graphsdk_Native_getConfig<'local>(
-	mut _env: JNIEnv<'local>,
+	env: JNIEnv<'local>,
 	_class: JClass<'local>,
-	_environment: JByteArray,
+	environment: JByteArray,
 ) -> JByteArray<'local> {
-	unimplemented!("Java_io_amplica_graphsdk_Native_getConfig")
+	let result = panic::catch_unwind(|| {
+		let rust_environment = map_to_environment(&env, &environment).unwrap();
+		let config = rust_environment.get_config();
+		let result = serialize_config(&env, &config).unwrap();
+		result
+	});
+	result.unwrap_or(JByteArray::default())
 }
 
 #[no_mangle]
 pub unsafe extern "C" fn Java_io_amplica_graphsdk_Native_containsUserGraph<'local>(
 	mut _env: JNIEnv<'local>,
 	_class: JClass<'local>,
-	_handle: jlong,
-	_dsnp_user_id: jlong,
+	handle: jlong,
+	dsnp_user_id: jlong,
 ) -> jboolean {
-	unimplemented!("Java_io_amplica_graphsdk_Native_containsUserGraph")
+	let result = panic::catch_unwind(|| {
+		if handle == 0 {
+			return JNI_FALSE
+		}
+		let graph_states = GRAPH_STATES.lock().unwrap();
+		if !graph_states.contains(&handle) {
+			return JNI_FALSE
+		}
+		let graph = unsafe { Box::from_raw(handle as *mut GraphState) };
+		// TODO: test edge case that dsnp_user_id is bigger than i64
+		let user_id = u64::try_from(dsnp_user_id).unwrap();
+		let result = graph.deref().contains_user_graph(&user_id).into();
+		// pulling out of the box as raw so that memory stays allocated
+		let _ = Box::into_raw(graph) as jlong;
+		result
+	});
+	result.unwrap_or(JNI_FALSE)
 }
 
 #[no_mangle]
 pub unsafe extern "C" fn Java_io_amplica_graphsdk_Native_getGraphUsersLength<'local>(
 	mut _env: JNIEnv<'local>,
 	_class: JClass<'local>,
-	_handle: jlong,
+	handle: jlong,
 ) -> jint {
-	unimplemented!("Java_io_amplica_graphsdk_Native_getGraphUsersLength")
+	let result = panic::catch_unwind(|| {
+		if handle == 0 {
+			return JNI_ERR
+		}
+		let graph_states = GRAPH_STATES.lock().unwrap();
+		if !graph_states.contains(&handle) {
+			return JNI_ERR
+		}
+		let graph = unsafe { Box::from_raw(handle as *mut GraphState) };
+		let result = graph.deref().len() as jint;
+		// pulling out of the box as raw so that memory stays allocated
+		let _ = Box::into_raw(graph) as jlong;
+		result
+	});
+	result.unwrap_or(JNI_ERR)
 }
 
 #[no_mangle]
 pub unsafe extern "C" fn Java_io_amplica_graphsdk_Native_removeUserGraph<'local>(
 	mut _env: JNIEnv<'local>,
 	_class: JClass<'local>,
-	_handle: jlong,
-	_dsnp_user_id: jlong,
+	handle: jlong,
+	dsnp_user_id: jlong,
 ) {
-	unimplemented!("Java_io_amplica_graphsdk_Native_removeUserGraph")
+	let _ = panic::catch_unwind(|| {
+		if handle == 0 {
+			return
+		}
+		let graph_states = GRAPH_STATES.lock().unwrap();
+		if !graph_states.contains(&handle) {
+			return
+		}
+		let mut graph = unsafe { Box::from_raw(handle as *mut GraphState) };
+		let user_id = u64::try_from(dsnp_user_id).unwrap();
+		graph.deref_mut().remove_user_graph(&user_id);
+		// pulling out of the box as raw so that memory stays allocated
+		let _ = Box::into_raw(graph) as jlong;
+	});
 }
 
 #[no_mangle]
 pub unsafe extern "C" fn Java_io_amplica_graphsdk_Native_importUserData<'local>(
-	mut _env: JNIEnv<'local>,
+	env: JNIEnv<'local>,
 	_class: JClass<'local>,
-	_handle: jlong,
-	_import: JByteArray,
-) -> JByteArray<'local> {
-	unimplemented!("Java_io_amplica_graphsdk_Native_importUserData")
+	handle: jlong,
+	imports: JByteArray,
+) {
+	let _ = panic::catch_unwind(|| {
+		if handle == 0 {
+			return
+		}
+		let graph_states = GRAPH_STATES.lock().unwrap();
+		if !graph_states.contains(&handle) {
+			return
+		}
+		let mut graph = unsafe { Box::from_raw(handle as *mut GraphState) };
+		let rust_imports = map_to_imports(&env, &imports).unwrap();
+		graph.deref_mut().import_users_data(&rust_imports).unwrap();
+		// pulling out of the box as raw so that memory stays allocated
+		let _ = Box::into_raw(graph) as jlong;
+	});
 }
 
 #[no_mangle]
 pub unsafe extern "C" fn Java_io_amplica_graphsdk_Native_exportUpdates<'local>(
-	mut _env: JNIEnv<'local>,
+	env: JNIEnv<'local>,
 	_class: JClass<'local>,
-	_handle: jlong,
+	handle: jlong,
 ) -> JByteArray<'local> {
-	unimplemented!("Java_io_amplica_graphsdk_Native_exportUpdates")
+	let result = panic::catch_unwind(|| {
+		if handle == 0 {
+			return JByteArray::default()
+		}
+		let graph_states = GRAPH_STATES.lock().unwrap();
+		if !graph_states.contains(&handle) {
+			return JByteArray::default()
+		}
+		let graph = unsafe { Box::from_raw(handle as *mut GraphState) };
+		let updates = graph.deref().export_updates().unwrap();
+		let result = serialize_graph_updates(&env, &updates).unwrap();
+		// pulling out of the box as raw so that memory stays allocated
+		let _ = Box::into_raw(graph) as jlong;
+		result
+	});
+	result.unwrap_or(JByteArray::default())
 }
 
 #[no_mangle]
 pub unsafe extern "C" fn Java_io_amplica_graphsdk_Native_applyActions<'local>(
-	mut _env: JNIEnv<'local>,
+	env: JNIEnv<'local>,
 	_class: JClass<'local>,
-	_handle: jlong,
-	_actions: JByteArray,
+	handle: jlong,
+	actions: JByteArray,
 ) {
-	unimplemented!("Java_io_amplica_graphsdk_Native_applyActions")
+	let _ = panic::catch_unwind(|| {
+		if handle == 0 {
+			return
+		}
+		let actions = map_to_actions(&env, &actions).unwrap();
+		let graph_states = GRAPH_STATES.lock().unwrap();
+		if !graph_states.contains(&handle) {
+			return
+		}
+		let mut graph = unsafe { Box::from_raw(handle as *mut GraphState) };
+		graph.deref_mut().apply_actions(&actions).unwrap();
+		// pulling out of the box as raw so that memory stays allocated
+		let _ = Box::into_raw(graph) as jlong;
+	});
 }
 
 #[no_mangle]
 pub unsafe extern "C" fn Java_io_amplica_graphsdk_Native_forceCalculateGraphs<'local>(
-	mut _env: JNIEnv<'local>,
+	env: JNIEnv<'local>,
 	_class: JClass<'local>,
-	_handle: jlong,
-	_dsnp_user_id: jlong,
+	handle: jlong,
+	dsnp_user_id: jlong,
 ) -> JByteArray<'local> {
-	unimplemented!("Java_io_amplica_graphsdk_Native_forceCalculateGraphs")
+	let result = panic::catch_unwind(|| {
+		if handle == 0 {
+			return JByteArray::default()
+		}
+		let graph_states = GRAPH_STATES.lock().unwrap();
+		if !graph_states.contains(&handle) {
+			return JByteArray::default()
+		}
+		let graph = unsafe { Box::from_raw(handle as *mut GraphState) };
+		let dsnp_user_id = DsnpUserId::try_from(dsnp_user_id).unwrap();
+		let updates = graph.deref().force_recalculate_graphs(&dsnp_user_id).unwrap();
+		let result = serialize_graph_updates(&env, &updates).unwrap();
+		// pulling out of the box as raw so that memory stays allocated
+		let _ = Box::into_raw(graph) as jlong;
+		result
+	});
+	result.unwrap_or(JByteArray::default())
 }
 
 #[no_mangle]
 pub unsafe extern "C" fn Java_io_amplica_graphsdk_Native_getConnectionsForUserGraph<'local>(
-	mut _env: JNIEnv<'local>,
+	env: JNIEnv<'local>,
 	_class: JClass<'local>,
-	_handle: jlong,
-	_dsnp_user_id: jlong,
-	_schema_id: jint,
-	_include_pending: jboolean,
+	handle: jlong,
+	dsnp_user_id: jlong,
+	schema_id: jint,
+	include_pending: jboolean,
 ) -> JByteArray<'local> {
-	unimplemented!("Java_io_amplica_graphsdk_Native_getConnectionsForUserGraph")
+	let result = panic::catch_unwind(|| {
+		if handle == 0 {
+			return JByteArray::default()
+		}
+		let graph_states = GRAPH_STATES.lock().unwrap();
+		if !graph_states.contains(&handle) {
+			return JByteArray::default()
+		}
+		let graph = unsafe { Box::from_raw(handle as *mut GraphState) };
+		let dsnp_user_id = DsnpUserId::try_from(dsnp_user_id).unwrap();
+		let schema_id = SchemaId::try_from(schema_id).unwrap();
+		let include_pending = convert_jboolean(include_pending).unwrap();
+		let graph_edges = graph
+			.deref()
+			.get_connections_for_user_graph(&dsnp_user_id, &schema_id, include_pending)
+			.unwrap();
+		let result = serialize_graph_edges(&env, &graph_edges).unwrap();
+		// pulling out of the box as raw so that memory stays allocated
+		let _ = Box::into_raw(graph) as jlong;
+		result
+	});
+	result.unwrap_or(JByteArray::default())
 }
 
 #[no_mangle]
 pub unsafe extern "C" fn Java_io_amplica_graphsdk_Native_getUsersWithoutKeys<'local>(
-	mut _env: JNIEnv<'local>,
+	env: JNIEnv<'local>,
 	_class: JClass<'local>,
-	_handle: jlong,
+	handle: jlong,
 ) -> JByteArray<'local> {
-	unimplemented!("Java_io_amplica_graphsdk_Native_getUsersWithoutKeys")
+	let result = panic::catch_unwind(|| {
+		if handle == 0 {
+			return JByteArray::default()
+		}
+		let graph_states = GRAPH_STATES.lock().unwrap();
+		if !graph_states.contains(&handle) {
+			return JByteArray::default()
+		}
+		let graph = unsafe { Box::from_raw(handle as *mut GraphState) };
+		let dsnp_users = graph.deref().get_connections_without_keys().unwrap();
+		let result = serialize_dsnp_users(&env, &dsnp_users).unwrap();
+		// pulling out of the box as raw so that memory stays allocated
+		let _ = Box::into_raw(graph) as jlong;
+		result
+	});
+	result.unwrap_or(JByteArray::default())
 }
 
 #[no_mangle]
 pub unsafe extern "C" fn Java_io_amplica_graphsdk_Native_getOneSidedPrivateFriendshipConnections<
 	'local,
 >(
-	mut _env: JNIEnv<'local>,
+	env: JNIEnv<'local>,
 	_class: JClass<'local>,
-	_handle: jlong,
-	_dsnp_user_id: jlong,
+	handle: jlong,
+	dsnp_user_id: jlong,
 ) -> JByteArray<'local> {
-	unimplemented!("Java_io_amplica_graphsdk_Native_getConnectionsForUserGraph")
+	let result = panic::catch_unwind(|| {
+		if handle == 0 {
+			return JByteArray::default()
+		}
+		let graph_states = GRAPH_STATES.lock().unwrap();
+		if !graph_states.contains(&handle) {
+			return JByteArray::default()
+		}
+		let graph = unsafe { Box::from_raw(handle as *mut GraphState) };
+		let user_id = u64::try_from(dsnp_user_id).unwrap();
+		let graph_edges =
+			graph.deref().get_one_sided_private_friendship_connections(&user_id).unwrap();
+		let result = serialize_graph_edges(&env, &graph_edges).unwrap();
+		// pulling out of the box as raw so that memory stays allocated
+		let _ = Box::into_raw(graph) as jlong;
+		result
+	});
+	result.unwrap_or(JByteArray::default())
 }
 
 #[no_mangle]
 pub unsafe extern "C" fn Java_io_amplica_graphsdk_Native_getPublicKeys<'local>(
-	mut _env: JNIEnv<'local>,
+	env: JNIEnv<'local>,
 	_class: JClass<'local>,
-	_handle: jlong,
-	_dsnp_user_id: jlong,
+	handle: jlong,
+	dsnp_user_id: jlong,
 ) -> JByteArray<'local> {
-	unimplemented!("Java_io_amplica_graphsdk_Native_getConnectionsForUserGraph")
+	let result = panic::catch_unwind(|| {
+		if handle == 0 {
+			return JByteArray::default()
+		}
+		let graph_states = GRAPH_STATES.lock().unwrap();
+		if !graph_states.contains(&handle) {
+			return JByteArray::default()
+		}
+		let graph = unsafe { Box::from_raw(handle as *mut GraphState) };
+		let user_id = u64::try_from(dsnp_user_id).unwrap();
+		let public_keys = graph.deref().get_public_keys(&user_id).unwrap();
+		let result = serialize_public_keys(&env, &public_keys).unwrap();
+		// pulling out of the box as raw so that memory stays allocated
+		let _ = Box::into_raw(graph) as jlong;
+		result
+	});
+	result.unwrap_or(JByteArray::default())
 }
