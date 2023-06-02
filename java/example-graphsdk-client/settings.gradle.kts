9047be770abeb3dc3e0abc821b00975524e607b7
Puneet Saraswat <puneet.saraswat10074@gmail.com>
2023-06-02 12:05:56 -0500

    fix settings

--- java/example-graphsdk-client/settings.gradle.kts
+++ java/example-graphsdk-client/settings.gradle.kts
@@ -7,4 +7,9 @@
  * in the user manual at https://docs.gradle.org/8.1.1/userguide/multi_project_builds.html
  */
 
+plugins {
+    // Apply the foojay-resolver plugin to allow automatic download of JDKs
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.4.0"
+}
+
 rootProject.name = "example-graphsdk-client"
