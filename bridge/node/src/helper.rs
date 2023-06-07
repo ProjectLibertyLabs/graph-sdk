54af611bf8c309a878271c2d35174c5476a16f26
Puneet Saraswat <puneet.saraswat10074@gmail.com>
2023-06-07 12:59:56 -0500

    simple placeholder examples

--- bridge/node/src/helper.rs
+++ bridge/node/src/helper.rs
@@ -1,7 +1,50 @@
-use dsnp_graph_config::{Config as RustConfig, DsnpVersion, MAINNET_CONFIG, ROCOCO_CONFIG};
+use dsnp_graph_config::{Config, DsnpVersion, Environment, SchemaConfig};
 use neon::{
-	context::Context, handle::Handle, prelude::FunctionContext, result::JsResult, types::JsObject,
+	handle::Handle,
+	object::Object,
+	prelude::{Context, FunctionContext},
+	result::JsResult,
+	types::{JsArray, JsNumber, JsObject, JsString, Value},
 };
 use std::collections::HashMap;
 
-// TODO implement neon helpers for these types
+/// Convert environment from JSObject to Environment
+/// # Arguments
+/// * `cx` - Neon FunctionContext
+/// * `environment_from_js` - Neon JsObject containing the environment
+/// # Returns
+/// * `JsResult<Environment>` - Neon Environment object
+/// # Errors
+/// * Throws a Neon error if the environment cannot be converted
+/// # Safety
+pub unsafe fn environment_from_js(
+	mut cx: FunctionContext,
+	environment_from_js: Handle<JsObject>,
+) -> Environment {
+	let environment_type_str: Handle<JsString> =
+		environment_from_js.get(&mut cx, "environmentType").unwrap_or(cx.string(""));
+
+	match environment_type_str.value().as_str() {
+		"mainnet" => Environment::Mainnet,
+		"rococo" => Environment::Rococo,
+		"dev" => {
+			let config: Handle<JsObject> =
+				environment_from_js.get(&mut cx, "config").unwrap_or(cx.empty_object());
+			let config = config_from_js(&mut cx, config);
+			Environment::Dev(config)
+		},
+		_ => Environment::Rococo,
+	}
+}
+
+/// Convert config from JSObject to Config
+/// # Arguments
+/// * `cx` - Neon FunctionContext
+/// * `config_from_js` - Neon JsObject containing the config
+/// # Returns
+/// * `Config` - Config object
+pub fn config_from_js(cx: &mut FunctionContext, config_from_js: Handle<JsObject>) -> Config {
+	let config_str: Handle<JsString> = config_from_js.to_string(cx).unwrap_or(cx.string(""));
+	let config_str = config_str.value();
+	Config::try_from(config_str.as_str()).unwrap()
+}
