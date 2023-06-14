6316a8ddb2cd247a05fa3f76248b1fecb5aef575
Puneet Saraswat <puneet.saraswat10074@gmail.com>
2023-06-14 12:26:09 -0500

    add some api logic

--- bridge/node/src/api.rs
+++ bridge/node/src/api.rs
@@ -1,6 +1,6 @@
 use crate::helper::*;
-use dsnp_graph_config::Config;
-use dsnp_graph_core::api::api::GraphState;
+use dsnp_graph_config::{Config, DsnpUserId};
+use dsnp_graph_core::api::api::{GraphState, GraphAPI};
 use neon::prelude::*;
 use once_cell::sync::Lazy;
 use std::{
@@ -122,6 +122,84 @@ pub fn get_graph_capacity(mut cx: FunctionContext) -> JsResult<JsNumber> {
 
 	Ok(cx.number(capacity as f64))
 }
+/// Get graph users count for given graph state
+/// # Arguments
+/// * `cx` - Neon FunctionContext
+/// * `graph_state_id` - Unique identifier for the graph state
+/// # Returns
+/// * `JsResult<JsNumber>` - Neon JsNumber containing the graph users count
+/// # Errors
+/// * Throws a Neon error if the graph state cannot be found
+pub fn get_graph_users_count(mut cx: FunctionContext) -> JsResult<JsNumber> {
+	let graph_state_id = cx.argument::<JsNumber>(0)?;
+	let graph_state_id = graph_state_id.value(&mut cx) as usize;
+
+	let states = GRAPH_STATES.lock().unwrap();
+	let graph_state = states.get(&graph_state_id);
+	if graph_state.is_none() {
+		return cx.throw_error("Graph state not found")
+	}
+	let graph_state = graph_state.unwrap();
+	let graph_state = graph_state.lock().unwrap();
+	let users_count = graph_state.len();
+
+	Ok(cx.number(users_count as f64))
+}
+
+/// Check if graph contains user
+/// # Arguments
+/// * `cx` - Neon FunctionContext
+/// * `graph_state_id` - Unique identifier for the graph state
+/// * `dsnpt_user_id` - DSNP user id
+/// # Returns
+/// * `JsResult<JsBoolean>` - Neon JsBoolean
+/// # Errors
+/// * Throws a Neon error if the graph state cannot be found
+pub fn contains_user_graph(mut cx: FunctionContext) -> JsResult<JsBoolean> {
+	let graph_state_id = cx.argument::<JsNumber>(0)?;
+	let graph_state_id = graph_state_id.value(&mut cx) as usize;
+	let dsnp_user_id = cx.argument::<JsNumber>(1)?;
+	let dsnp_user_id = dsnp_user_id.value(&mut cx) as DsnpUserId;
+
+	let states = GRAPH_STATES.lock().unwrap();
+	let graph_state = states.get(&graph_state_id);
+	if graph_state.is_none() {
+		return cx.throw_error("Graph state not found")
+	}
+	let graph_state = graph_state.unwrap();
+	let graph_state = graph_state.lock().unwrap();
+	let contains_user = graph_state.contains_user_graph(&dsnp_user_id);
+
+	Ok(cx.boolean(contains_user))
+}
+
+
+/// Function to remove user graph from the graph state
+/// # Arguments
+/// * `cx` - Neon FunctionContext
+/// * `graph_state_id` - Unique identifier for the graph state
+/// * `dsnp_user_id` - DSNP user id
+/// # Returns
+/// * `JsResult<JsUndefined>` - Neon JsUndefined
+/// # Errors
+/// * Throws a Neon error if the graph state cannot be found
+pub fn remove_user_graph(mut cx: FunctionContext) -> JsResult<JsUndefined> {
+	let graph_state_id = cx.argument::<JsNumber>(0)?;
+	let graph_state_id = graph_state_id.value(&mut cx) as usize;
+	let dsnp_user_id = cx.argument::<JsNumber>(1)?;
+	let dsnp_user_id = dsnp_user_id.value(&mut cx) as DsnpUserId;
+
+	let mut states = GRAPH_STATES.lock().unwrap();
+	let graph_state = states.get_mut(&graph_state_id);
+	if graph_state.is_none() {
+		return cx.throw_error("Graph state not found")
+	}
+	let graph_state = graph_state.unwrap();
+	let mut graph_state = graph_state.lock().unwrap();
+	graph_state.remove_user_graph(&dsnp_user_id);
+
+	Ok(cx.undefined())
+}
 
 /// Function to free the graph state
 /// # Arguments
@@ -154,6 +232,9 @@ fn main(mut cx: ModuleContext) -> NeonResult<()> {
 	cx.export_function("initializeGraphState", initialize_graph_state)?;
 	cx.export_function("initializeGraphStateWithCapacity", initialize_graph_state_with_capacity)?;
 	cx.export_function("getGraphCapacity", get_graph_capacity)?;
+	cx.export_function("getGraphUsersCount", get_graph_users_count)?;
+	cx.export_function("containsUserGraph", contains_user_graph)?;
+	cx.export_function("removeUserGraph", remove_user_graph)?;
 	cx.export_function("freeGraphState", free_graph_state)?;
 	Ok(())
 }
