eb3f1e18c2b59876a4328667195dab6b6d2e092a
Puneet Saraswat <puneet.saraswat10074@gmail.com>
2023-06-21 10:20:53 -0500

    add example client

---
+++ bridge/node/node-example-client/index.ts
@@ -0,0 +1,36 @@
+import { Graph, EnvironmentInterface, Action, DsnpKeys, EnvironmentType, ConnectAction, Connection, ConnectionType, PrivacyType } from "@dsnp/graph-sdk";
+
+const environment: EnvironmentInterface =  {environmentType: EnvironmentType.Mainnet};
+const graph = new Graph(environment);
+
+async function interactWithGraph() {
+    let public_follow_graph_schema_id = await graph.getSchemaIdFromConfig(environment, ConnectionType.Follow, PrivacyType.Public);
+
+    let connect_action: ConnectAction = {
+        type: "Connect",
+        ownerDsnpUserId: 1,
+        connection: {
+            dsnpUserId: 2,
+            schemaId: public_follow_graph_schema_id,
+        } as Connection,
+        dsnpKeys: {
+            dsnpUserId: 2,
+            keysHash: 100,
+            keys: [],
+        } as DsnpKeys,
+    } as ConnectAction;
+    
+    let actions = [] as Action[];
+    actions.push(connect_action);
+
+    let applied = await graph.applyActions(actions);
+    console.log(applied);
+
+    let connections_including_pending = await graph.getConnectionsForUserGraph(1, public_follow_graph_schema_id, true);
+    console.log(connections_including_pending);
+
+    let exported = await graph.exportUpdates();
+    console.log(exported);
+}
+  
+interactWithGraph();
