e7ce308057b8e02937df5bc1d1e2f4c135265382
Puneet Saraswat <puneet.saraswat10074@gmail.com>
2023-07-16 20:52:51 -0500

    add test for api

--- bridge/node/js/import-bundle-builder.spec.ts
+++ bridge/node/js/import-bundle-builder.spec.ts
@@ -43,4 +43,24 @@ describe('ImportBundleBuilder', () => {
       pages,
     });
   });
+
+  it('should build the import bundle correctly with withGraphKeyPair', () =>{
+    const dsnpUserId = "111";
+    const schemaId = 123;
+
+    const importBundleBuilder = new ImportBundleBuilder()
+      .withDsnpUserId(dsnpUserId)
+      .withSchemaId(schemaId)
+      .withGraphKeyPair(GraphKeyType.X25519, new Uint8Array([1, 2, 3]), new Uint8Array([4, 5, 6]))
+      .withDsnpKeys({dsnpUserId: "111", keysHash: 456, keys: [{index: 0, content: new Uint8Array([7, 8, 9])}]})
+      .withPageData(1, new Uint8Array([10, 11, 12]), 789);
+
+    expect(importBundleBuilder.build()).toEqual({
+      dsnpUserId,
+      schemaId,
+      keyPairs: [{keyType: GraphKeyType.X25519, publicKey: new Uint8Array([1, 2, 3]), secretKey: new Uint8Array([4, 5, 6])} as GraphKeyPair],
+      dsnpKeys: {dsnpUserId: "111", keysHash: 456, keys: [{index: 0, content: new Uint8Array([7, 8, 9])}]},
+      pages: [{pageId: 1, content: new Uint8Array([10, 11, 12]), contentHash: 789}],
+    });
+  });
 });
