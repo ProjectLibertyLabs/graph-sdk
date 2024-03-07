2c6fe8c3d235d22047efda8269759aed9dbe0350
Aramik <aramikm@gmail.com>
2024-03-06 17:55:17 -0800

    fix tests

--- scripts/install_jni.sh
+++ scripts/install_jni.sh
@@ -7,7 +7,7 @@ JNI_NAME=dsnp_graph_sdk_jni
 
 install -d ${BACKEND_LIB_DIR}
 for possible_library_name in "lib${JNI_NAME}.dylib" "lib${JNI_NAME}.so" "${JNI_NAME}.dll"; do
-  possible_library_path="target/release/${possible_library_name}"
+  possible_library_path="target/production/${possible_library_name}"
   if [ -e "${possible_library_path}" ]; then
     echo "Installing ${possible_library_name} to ${BACKEND_LIB_DIR}"
     install "${possible_library_path}" "${BACKEND_LIB_DIR}"
