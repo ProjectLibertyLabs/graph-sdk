e713efd8f1361ef77ee7e5a065bb3510db288a3f
Wil Wade <wil.wade@amplica.io>
2024-05-03 12:23:40 -0400

    Update Links (#203)
    # Goal
    The goal of this PR is to update out of date links.

--- core/src/frequency/mod.rs
+++ core/src/frequency/mod.rs
@@ -1,4 +1,4 @@
-//! Defines and implements [Frequency](https://github.com/LibertyDSNP/frequency) specific data types
+//! Defines and implements [Frequency](https://github.com/frequency-chain/frequency) specific data types
 pub mod reader_writer;
 
 /// A utility to read/write data from and to Frequency chain specific implementation of DSNP
